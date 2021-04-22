33a4e9f483288ad31690dcf2ade4b366d164fa8f
Trevor <trevor@osrs-stat.com>
2021-04-22 23:43:23 +0900

    Add initial calculate module

--- lib/apy.rb
+++ lib/apy.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "apy/calculate"
+require_relative "apy/interest"
 require_relative "apy/version"
 
 module Apy
--- test/apy_test.rb
+++ test/apy_test.rb
@@ -3,11 +3,25 @@
 require "test_helper"
 
 class ApyTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Apy::VERSION
+  include Apy::Calculate
+
+  def test_weighted_harmonic_mean
+    inputs = [
+      [1000, 156.23],
+      [1000, 156.30],
+      [1000, 173.15],
+      [1000, 188.72],
+      [1000, 204.61],
+      [1000, 178.23]
+    ]
+    actual = weighted_harmonic_mean(inputs)
+
+    assert_in_delta 174.57, actual, 0.01
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_compound
+    actual = compound(1200, apy: 0.1, times: 12, terms: 1)
+
+    assert_in_delta 1325.66, actual, 0.01
   end
 end
